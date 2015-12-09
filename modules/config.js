6f834fe9fdea863d054d7290220f697b566a1c85
Masafumi Yokoyama <yokoyama@clear-code.com>
2015-12-09 14:48:28 +0900

    Remove needless modules

--- modules/config.js
+++
@@ -1,3 +0,0 @@
-var config = require('lib/config');
-config.setDefault('extensions.restartless@piro.sakura.ne.jp.testBoolean', true);
-config.setDefault('extensions.restartless@piro.sakura.ne.jp.testInteger', 10);
