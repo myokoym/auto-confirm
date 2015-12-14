7f21eeb1d7a89a03bb4447733b648a6883052435
Masafumi Yokoyama <yokoyama@clear-code.com>
2015-12-14 11:20:05 +0900

    Cleanup

--- modules/main.js
+++ modules/main.js
@@ -1,14 +1,3 @@
-/**
- * @fileOverview Main module for restartless addons
- * @author       YUKI "Piro" Hiroshi
- * @version      4
- *
- * @license
- *   The MIT License, Copyright (c) 2015 YUKI "Piro" Hiroshi.
- *   https://github.com/piroor/restartless/blob/master/license.txt
- * @url http://github.com/piroor/restartless
- */
-
 var BASE = 'extensions.auto-confirm@myokoym.net.';
 var prefs = require('lib/prefs').prefs;
 
@@ -18,16 +7,8 @@ function log(message) {
   }
 }
 
-/**
- * load() works like Components.utils.import(). EXPORTED_SYMBOLS
- * in loaded scripts are exported to the global object of this script.
- */
 load('lib/WindowManager');
 
-/**
- * Sample code for addons around browser windows.
- */
-const TYPE_BROWSER = 'navigator:browser';
 var global = this;
 function handleWindow(aWindow)
 {
@@ -52,13 +33,8 @@ function handleCommonDialog(aWindow)
 
 WindowManager.addHandler(handleWindow);
 
-/**
- * A handler for shutdown event. This will be called when the addon
- * is disabled or uninstalled (include updating).
- */
 function shutdown()
 {
-	// free loaded symbols
 	WindowManager = undefined;
 	global = undefined;
 }
