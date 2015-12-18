9de64a953c9d7064061fdaf2acf2fac7d47a0137
YUKI Hiroshi <shimoda@clear-code.com>
2015-12-18 16:38:15 +0900

    Add UI to configure actions

--- content/edit.js
+++ content/edit.js
@@ -5,9 +5,12 @@
 */
 
 var gRule;
+var gActions;
 
 function init() {
   gRule = window.arguments[0] || {};
+
+  gActions = document.getElementById('actions');
 
   Object.keys(gRule).forEach(function(aKey) {
     var value = gRule[aKey];
@@ -16,23 +19,55 @@ function init() {
       field.value = value;
   });
 
+  var actions = [];
+  if (gRule.actions)
+    actions = JSON.parse(gRule.actions);
+  else if (gRule.action)
+    actions = [gRule.action];
+
+  actions.forEach(actionAdd);
+
   onGroupChanged();
 }
 
 function onGroupChanged() {
   var group = document.getElementById('group-field').value;
-  Array.forEach(document.querySelectorAll('.group-common'), function(aItem) {
-    if (group == 'common')
-      aItem.removeAttribute('hidden');
-    else
-      aItem.setAttribute('hidden', true);
-  });
-  Array.forEach(document.querySelectorAll('.group-general'), function(aItem) {
-    if (group == 'general')
-      aItem.removeAttribute('hidden');
-    else
-      aItem.setAttribute('hidden', true);
-  });
+  document.documentElement.setAttribute('group-type', group);
+}
+
+function actionAdd(aAction) {
+  var template = document.getElementById('action-item-template');
+  var item = template.cloneNode(true);
+  item.removeAttribute('id');
+  gActions.insertBefore(item, gActions.lastChild);
+
+  aAction = aAction || '';
+  var matched = aAction.match(/^([^;]+)(?:;(.*)$)?/);
+  if (!matched)
+    return;
+
+  var name = matched[1];
+  var params = matched[2];
+  if (name)
+    item.querySelector('.action-name-field').value = name;
+  if (params)
+    item.querySelector('.action-params-field').value = params;
+}
+
+function actionUp(aItem) {
+  if (aItem == gActions.firstChild)
+    return;
+  gActions.isnertBefore(aItem, aItem.previousSibling);
+}
+
+function actionDown(aItem) {
+  if (aItem.nextSibling == gActions.lastChild)
+    return;
+  gActions.isnertBefore(aItem, aItem.nextSibling);
+}
+
+function actionDelete(aItem) {
+  gActions.removeChild(aItem);
 }
 
 function onAccept() {
